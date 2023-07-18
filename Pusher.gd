a4364172cf9b1eb204577a60a7f2e215e1b6ca70
diegoesp89 <diego.espinoza.vasquez@gmail.com>
2023-07-18 18:33:07 -0400

    Upd new prefabs & configs

--- Pusher.gd
+++ Pusher.gd
@@ -1,15 +1,18 @@
-extends CSGBox3D
+extends AnimatableBody3D
 
 @export var mov = 0.0
-@export var rate = 0.01
+@export var rate = 0.025
+@export var top_range = 10.0
+@export var bot_range = 0.0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if(mov > 1.7 or mov < 0.0):
+func _process(_delta):
+	if(mov > top_range or mov < bot_range):
 		rate *= -1.0
 	self.position.z += rate
 	mov += rate
