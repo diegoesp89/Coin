668e34bcc766ac58e4f537040ae073058fe1f574
Strife <diego.espinoza.vasquez@gmail.com>
2023-07-22 21:48:20 -0400

    Shrink scenery & set spawner

--- Scripts/CoinSpawner.gd
+++ Scripts/CoinSpawner.gd
@@ -1,15 +1,21 @@
 extends Node
 
 @export var coin_prefab = load("res://Prefabs/coin.tscn")
-@export var level: PackedScene = load("res://Prefabs/scene1.tscn")
 @export var count = 0
-@export var Shooter: Node
+@export var spawner: Node
 
 func spawn():
+	var pos_x_spawner = spawner.position.x
+	var pos_y_spawner = spawner.position.y
+	var pos_z_spawner = spawner.position.z
 	var coin = coin_prefab.instantiate()
+	coin.position.x = pos_x_spawner
+	coin.position.y = pos_y_spawner
+	coin.position.z = pos_z_spawner
 	add_child(coin)
 	count+=1
 	print(count)
+
 	
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,6 +24,6 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	if Input.is_key_pressed(KEY_SPACE):
+	if Input.is_action_just_released("spawn_button"):
 		spawn()
 	pass
--- Scripts/CoinSpawnerMove.gd
+++ Scripts/CoinSpawnerMove.gd
@@ -1,17 +1,15 @@
 extends Node
 
 @export var mov = 0
-@export var min_dist = -3
-@export var max_dist = 3
+@export var min_dist = -2
+@export var max_dist = 2
 @export var speed = 0.01
 @export var x_pos = 0
 
 
-signal set_pos_x()
 	
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_ṕos_x.emit()
 	x_pos = self.position.x
 	pass # Replace with function body.
 
@@ -21,6 +19,5 @@ func _process(_delta):
 	if(mov > max_dist or mov < min_dist):
 		speed *= -1.0
 	self.position.x += speed
-	set_pos_x()
 	mov += speed
 	pass
