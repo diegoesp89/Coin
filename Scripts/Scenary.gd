a89e020a0a3f34a766c5f532d9b210a09bbb45aa
strife <strife@customer.sntochl1.pop.starlinkisp.net>
2026-02-17 01:14:41 -0300

    Apply wall textures via code

---
+++ Scripts/Scenary.gd
@@ -0,0 +1,17 @@
+extends Node
+
+func _ready():
+	_apply_wall_textures()
+
+func _apply_wall_textures():
+	for child in get_children():
+		if child is CSGBox3D and child.name.begins_with("Wall"):
+		var mat = StandardMaterial3D.new()
+		mat.albedo_texture = load("res://Materials/wall_albedo.png")
+		mat.normal_texture = load("res://Materials/wall_normal.png")
+		mat.normal_enabled = true
+		mat.roughness_texture = load("res://Materials/wall_roughness.png")
+		mat.roughness_enabled = true
+		mat.height_texture = load("res://Materials/wall_height.png")
+		mat.height_enabled = true
+		child.material_override = mat
